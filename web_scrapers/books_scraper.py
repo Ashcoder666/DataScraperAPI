83ac3fb9c2f9532b2a57e7ee958d8d8c2c3cf4e0
Ashcoder666 <ashirnbasheer@gmail.com>
2025-09-04 21:40:14 +0530

    bookscraper completed

--- web_scrapers/books_scraper.py
+++ web_scrapers/books_scraper.py
@@ -1,27 +1,44 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+import pandas as pd
+
+
 
 base_url = "http://books.toscrape.com/"
-url=base_url
 
-while url:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    books = soup.find_all("article")
 
-    for book in books:
-        title = book.h3.a["title"]
-        bookPrice = book.find("p",class_="price_color").text
-        print(f"Book title : {title} , Price : {bookPrice}")
-    
-    next_button = soup.find("li",class_="next")
-    if next_button:
-        next_page = next_button.a["href"]
-        url = urljoin(url, next_page)
-        # print(url)
-    else:
-        url=None
+def bookScraper():
+    url=base_url
+    allBooks = []
+    current_percentage = 2
+
+    while url:
+        response = requests.get(url,timeout=15)
+        soup = BeautifulSoup(response.text, "html.parser")
+        books = soup.find_all("article")
+
+        for book in books:
+            title = book.h3.a["title"]
+            bookPrice = book.find("p",class_="price_color").text
+            allBooks.append({"title":title,"price":bookPrice})
         
+    
+        next_button = soup.find("li",class_="next")
+        if next_button:
+            next_page = next_button.a["href"]
+            url = urljoin(url, next_page)
+        else:
+            url=None
+        
+    
+        print(f"{current_percentage}% is completed" )
+        current_percentage +=  2
 
+    df = pd.DataFrame(allBooks)
 
+    df["price"] = df["price"].str.replace("£", "").astype(float)
+
+    df.to_csv("books.csv",index=False)
+    
+    
